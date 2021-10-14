89d1ebac0a17a44f0aca17e384f250536261d07f
Martin Macecek <martin.macecek@gmx.net>
2021-10-14 10:26:10 -0700

    readme and small correction to parameter validation

--- src/CfnReportsToJunitXml.py
+++ src/CfnReportsToJunitXml.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import argparse, json, sys, re, pathlib, bisect
+import argparse, json, sys, re, pathlib, logging, os
 sys.path.insert(0, "external")
 from junit_xml import TestSuite, TestCase
 from os import walk
@@ -36,12 +36,26 @@ parser.add_argument('reportType', choices=['CFN-NAG', 'CFN-GUARD', 'CFN-LINT'], 
 parser.add_argument('output', type=xml_filename_regex, help='The output xml file name of the generated JSON configuration')
 parser.add_argument('--rules', help='The file name of the rules file')
 parser.add_argument('--pathToTemplates', help='The path to the CloudFormation templates')
+parser.add_argument('--logLevel', choices=['critical', 'error', 'warn', 'warning', 'info', 'debug'], help='A valid log level. If none provided, info is used')
 args = parser.parse_args()
 
 if (args.reportType == 'CFN-NAG' or args.reportType == 'CFN-LINT') and args.rules is None:
     raise ValueError(f"For reportType {args.reportType}, parameter rules must be provided")
-if (args.reportType == 'CFN-LINT') and args.rules is None:
+if (args.reportType == 'CFN-LINT') and args.pathToTemplates is None:
     raise ValueError(f"For reportType {args.reportType}, parameter pathToTemplates must be provided")
+
+logLevel = 'info' if args.logLevel is None else args.logLevel
+logging.basicConfig()
+logger = logging.getLogger()
+levels = {
+    'critical': logging.CRITICAL,
+    'error': logging.ERROR,
+    'warn': logging.WARNING,
+    'warning': logging.WARNING,
+    'info': logging.INFO,
+    'debug': logging.DEBUG
+}
+logger.setLevel(levels.get(logLevel.lower()))
 
 def write_junit_xml_report(rules, report):
     test_cases = []
@@ -50,8 +64,6 @@ def write_junit_xml_report(rules, report):
             test_case=TestCase(f"{rule['id']} - {file_findings['filename']}")
             violations = [v for v in file_findings["file_results"]['violations'] if v['id'] == rule['id']] 
             if violations:
-                # TODO
-                print(f"Violations: {violations}")
                 if 'detailMessages' in violations[0]:
                     output=",\n".join(list(map(lambda x, y, z: f"Line {x} ({y}): {z}", violations[0]['line_numbers'], violations[0]['logical_resource_ids'], violations[0]['detailMessages'])))
                 else:
@@ -88,6 +100,7 @@ def insert_rule(rules, id, type, message):
 def generate_junit_report_from_cfn_lint():
     pattern = re.compile(r"^(?P<id>[EW]{1}\d+)\: (?P<message>.*)$")
     rules = []
+    # This rule is not part of the rules file, but it is a commonly tested for rule and is therefore hardcoded
     rules.append({"id": "E0000", "type": "FAIL", "message": "Parsing error found when parsing the template"})
     with open(args.rules, 'r') as stream:
         for line in stream:  
@@ -115,26 +128,25 @@ def generate_junit_report_from_cfn_lint():
             logical_resource_id = finding['Location']['Path'][1]
         else:
             match = rPattern.match(detailMessage)
-            print(f"detailMessage: {detailMessage}, match: {match}")
             logical_resource_id = match.group('resourceId') if match else "NA"
         line_number = finding['Location']['Start']['LineNumber']
         file = [f for f in report if f['filename'] == filename ]
         if file:
-            print(f"Filename {filename} already exists; adding violation")
+            logger.debug(f"Filename {filename} already exists; adding violation")
             violations = file[0]['file_results']['violations'] 
             violation = [v for v in violations if v['id'] == id]
             if violation:
-                print(f"Violation {violation[0]['id']} already exists; adding details to violation")
+                logger.debug(f"Violation {violation[0]['id']} already exists; adding details to violation")
                 violation[0]['logical_resource_ids'].append(logical_resource_id)
                 violation[0]['line_numbers'].append(line_number)
                 violation[0]['detailMessages'].append(detailMessage)
             else:
-                print(f"Violation for Rule {id} does not yet exist; adding new violation")
+                logger.debug(f"Violation for Rule {id} does not yet exist; adding new violation")
                 file[0]['file_results']['failure_count'] += 1
                 violations.append({"id": id, "type": type, "message": message, "detailMessages": [detailMessage], "logical_resource_ids": [logical_resource_id], "line_numbers": [line_number]})
                 insert_rule(rules, id, type, message)
         else:
-            print(f"Filename {filename} does not yet exist; adding violation as first to new record")            
+            logger.debug(f"Filename {filename} does not yet exist; adding violation as first to new record")            
             report.append({
                 "filename": filename,
                 "file_results": {"failure_count": 1, "violations": [{"id": id, "type": type, "message": message, "detailMessages": [detailMessage], "logical_resource_ids": [logical_resource_id], "line_numbers": [line_number]}]}})
@@ -174,12 +186,12 @@ def process_report():
     # elif args.reportType == 'CFN-GUARD':            
     #     return generate_junit_report_from_cfn_guard()
     else:
-        print(f"Not yet supported report type: {args.reportType}")
+        logger.error(f"Not yet supported report type: {args.reportType}")
 
-try:
-    print("Starting function") 
+try:    
+    logger.info("Starting function") 
     process_report()
-    print("Finished function")
+    logger.info("Finished function")
 except Exception as error:
-    print("Error {}".format(error))
+    logger.error(f"{error}")
     raise
